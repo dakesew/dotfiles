eadca62242a05431766679ec08833f651fab206f
Dake Sew <dakesew@yahoo.de>
2015-05-26 21:54:55 +0200

    Added default editor and added mlocate package for installed software

--- install_software.sh
+++ install_software.sh
@@ -1,3 +1,3 @@
 echo "Uses yaourt"
 
-yaourt -S i3-gaps-git dmenu xorg adobe-source-code-pro-fonts rofi emacs avrdude avr-gcc arm-none-eabi-gcc openocd gcc zsh firefox feh engrampa gnome-terminal avr-gdb gdb arm-none-eabi-gdb git lxdm make thunar transmission-gtk thunar-volman thunar-shares-plugin i3status 
+yaourt -S i3-gaps-git dmenu xorg adobe-source-code-pro-fonts rofi emacs avrdude avr-gcc arm-none-eabi-gcc openocd gcc zsh firefox feh engrampa gnome-terminal avr-gdb gdb arm-none-eabi-gdb git lxdm make thunar transmission-gtk thunar-volman thunar-shares-plugin i3status  mlocate
