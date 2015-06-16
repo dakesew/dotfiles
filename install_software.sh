679b110c496e4e0f39880714821c433ec9d95de5
David Sawatzke <david@ArchNetbookDavid.lan>
2015-06-16 22:32:16 +0200

    added --needed parameter in install_software.sh

--- install_software.sh
+++ install_software.sh
@@ -1,2 +1,2 @@
 echo "Uses yaourt"
-yaourt -S i3-gaps-git dmenu xorg xorg-xinit adobe-source-code-pro-fonts rofi emacs avrdude avr-gcc arm-none-eabi-gcc openocd gcc zsh zsh-completions firefox feh engrampa xterm avr-gdb gdb arm-none-eabi-gdb git lxdm make thunar transmission-gtk thunar-volman thunar-shares-plugin i3status  mlocate
+yaourt -S --needed i3-gaps-git dmenu xorg xorg-xinit adobe-source-code-pro-fonts rofi emacs avrdude avr-gcc arm-none-eabi-gcc openocd gcc zsh zsh-completions firefox feh engrampa xterm avr-gdb gdb arm-none-eabi-gdb git lxdm make thunar transmission-gtk thunar-volman thunar-shares-plugin i3status  mlocate
