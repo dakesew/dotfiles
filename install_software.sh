5e198746bb06d8e75487ac08b44d8ebd2ed83d21
Dake Sew <dakesew@yahoo.de>
2015-06-17 20:00:20 +0200

    Changed ZSH Configuration to grml

--- install_software.sh
+++ install_software.sh
@@ -1,2 +1,2 @@
 echo "Uses yaourt"
-yaourt -S --needed prezto-git xbindkeys i3-gaps-git xorg xorg-xinit adobe-source-code-pro-fonts rofi emacs avrdude avr-gcc arm-none-eabi-gcc openocd gcc zsh zsh-completions zsh-syntax-highlighting firefox feh engrampa xfce4-terminal avr-gdb gdb arm-none-eabi-gdb git lxdm make thunar transmission-gtk thunar-volman thunar-shares-plugin i3status  mlocate
+yaourt -S --needed xbindkeys i3-gaps-git xorg xorg-xinit adobe-source-code-pro-fonts rofi emacs avrdude avr-gcc arm-none-eabi-gcc openocd gcc zsh zsh-completions zsh-syntax-highlighting grml-zsh-config firefox feh engrampa xfce4-terminal avr-gdb gdb arm-none-eabi-gdb git lxdm make thunar transmission-gtk thunar-volman thunar-shares-plugin i3status  mlocate
