127c76d1298a6b51fc2829417be06b952e109050
Dake Sew <dakesew@yahoo.de>
2015-07-05 17:15:25 +0200

    Changed standard terminal to emacs multi-term, removed mlterm because it's no longer needed

--- install_software.sh
+++ install_software.sh
@@ -1,7 +1,7 @@
 echo "Uses yaourt"
 yaourt -S --needed xorg xorg-xinit xbindkeys lxdm \
 i3-gaps-git i3status i3lock python2 python2-imaging xpyb source-code-pro-fonts rofi feh \
-firefox engrampa mlterm thunar transmission-gtk thunar-volman thunar-shares-plugin \
+firefox engrampa thunar transmission-gtk thunar-volman thunar-shares-plugin \
 zsh zsh-completions zsh-syntax-highlighting grml-zsh-config \
 gcc git make mlocate gdb emacs \
 avr-gcc avr-gdb avrdude \
