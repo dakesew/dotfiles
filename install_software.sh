15acd1fb7df4b1d958b71e41e81458d9c2195c54
Dake Sew <dakesew@yahoo.de>
2015-07-06 16:41:23 +0200

    Recitfied name

--- install_software.sh
+++ install_software.sh
@@ -1,6 +1,6 @@
 echo "Uses yaourt"
 yaourt -S --needed xorg xorg-xinit xbindkeys lxdm \
-i3-gaps-git i3status i3lock python2 python2-imaging xpyb source-code-pro-fonts rofi feh \
+i3-gaps-git i3status i3lock python2 python2-imaging xpyb adobe-source-code-pro-fonts rofi feh \
 firefox engrampa thunar transmission-gtk thunar-volman thunar-shares-plugin \
 zsh zsh-completions zsh-syntax-highlighting grml-zsh-config \
 gcc git make mlocate gdb emacs \
