2ae945119ba391751e103516189fcf0fa4865e9c
Dake Sew <dakesew@yahoo.de>
2016-02-13 14:09:43 +0100

    Better empty lines

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -43,6 +43,7 @@ values."
      python
      spacemacs-layouts
      evil-snipe
+     vim-empty-lines
      )
    ;; List of additional packages that will be installed without being
    ;; wrapped in a layer. If you need some configuration for these
