6223a6bb8f060f256df4949192ee0e82a900d38d
David Sawatzke <david@sawatzk.de>
2016-08-06 15:06:42 +0200

    Move wallpapers out of git

--- i3/wallpaper.sh
+++ i3/wallpaper.sh
@@ -2,8 +2,7 @@
 
 while :
 do
-    #wallpaper=$(shuf -n1 -e $(shuf -n1 -e ~/.i3/Wallpapers/X*/ ~/.i3/Wallpapers/XX*/)*)
-    wallpaper=$(shuf -n1 -e ~/.i3/Wallpapers/X*/*)
+    wallpaper=$(shuf -n1 -e ~/Portable/Wallpapers/X*/*)
     feh --no-fehbg --bg-fill "$wallpaper"
     echo "$wallpaper" > /tmp/wallpaper
     sleep $1
