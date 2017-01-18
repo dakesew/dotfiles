96df80178ec8bec9e7bf0687555c2109b276a821
David Sawatzke <david@sawatzk.de>
2017-01-18 07:33:54 +0100

    Add rainbow-mode package to emacs

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -84,7 +84,7 @@ values."
    ;; wrapped in a layer. If you need some configuration for these
    ;; packages, then consider creating a layer. You can also put the
    ;; configuration in `dotspacemacs/user-config'.
-   dotspacemacs-additional-packages '(editorconfig eimp epresent mingus)
+   dotspacemacs-additional-packages '(editorconfig eimp epresent mingus rainbow-mode)
    ;; A list of packages that cannot be updated.
    dotspacemacs-frozen-packages '()
    ;; A list of packages that will not be installed and loaded.
