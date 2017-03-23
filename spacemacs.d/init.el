34706901f1351fe235bd0d95f69f0c9d610d6996
David Sawatzke <david@sawatzk.de>
2017-03-23 18:48:08 +0100

    Add spacemacs evil
    Primarily for evil-surround

--- spacemacs.d/init.el
+++ spacemacs.d/init.el
@@ -31,6 +31,7 @@ values."
    ;; List of configuration layers to load.
    dotspacemacs-configuration-layers
    '(
+     spacemacs-evil
      ivy
      (auto-completion :disabled-for
                       org
