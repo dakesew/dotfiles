101f3f6700d2304e4641e820abd7ebd450b2c3f7
David Sawatzke <david@sawatzk.de>
2017-12-26 20:18:01 +0100

    Add custom settings for calc

---
+++ spacemacs.d/calc.el
@@ -0,0 +1,30 @@
+
+;;; Mode settings stored by Calc on Tue Dec 26 20:15:42 2017
+(setq calc-show-banner nil)
+(setq calc-full-mode t)
+(setq calc-window-height 34)
+(setq calc-language 'big)
+(setq calc-display-strings t)
+(setq calc-full-float-format '(eng 0))
+(setq calc-float-format '(eng 4))
+(setq calc-group-char "_")
+(setq calc-group-digits t)
+;;; End of mode settings
+
+;;; Custom units stored by Calc on Fri Mar  6 14:10:18 2015
+(setq math-additional-units '(
+			      (bits nil "bits")
+			      (bytes "8 * bits" "bytes")
+
+			      (tib "1000 * gib"    "TiB")
+			      (gib "1000 * mib"    "GiB")
+			      (mib "1000 * kib"    "MiB")
+			      (kib "1000 * bytes"  "KiB")
+
+			      (tb  "1024 * gb"      "TB")
+			      (gb  "1024 * mb"      "GB")
+			      (mb  "1024 * kb"      "MB")
+			      (kb  "1024 * bytes"   "KB")
+			      ))
+;;; End of custom units
+
